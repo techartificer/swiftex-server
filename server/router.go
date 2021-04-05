2a10505108001f0c397a928189bddf3ab60fed15
caffeines <sadat.talks@gmail.com>
2021-04-05 17:20:04 +0600

    added pprof

--- server/router.go
+++ server/router.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"net/http"
+	_ "net/http/pprof"
+	"net/url"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -11,6 +13,16 @@ import (
 
 var router = echo.New()
 
+func wrapHandler(h http.Handler) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		s := strings.TrimRight(c.Request().URL.String(), "/")
+		c.Request().URL = &url.URL{Path: s, Host: c.Request().Host}
+		c.Request().RequestURI = strings.TrimRight(c.Request().RequestURI, "/")
+		h.ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
+}
+
 // GetRouter returns the api router
 func GetRouter() http.Handler {
 	router.Use(middleware.GzipWithConfig(middleware.GzipConfig{
@@ -19,8 +31,6 @@ func GetRouter() http.Handler {
 			return strings.Contains(ctx.Path(), "/fs/") || strings.Contains(ctx.Path(), "/download/")
 		},
 	}))
-
-	router.Pre(middleware.AddTrailingSlash())
 	router.Use(middleware.Recover())
 	// router.Use(echoMonitoring())
 	router.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
@@ -32,9 +42,12 @@ func GetRouter() http.Handler {
 		AllowMethods: []string{"*"},
 	}))
 
+	router.Pre(middleware.AddTrailingSlash())
 	router.GET("/", func(ctx echo.Context) error {
 		return ctx.JSON(http.StatusOK, map[string]string{"health": "OK"})
 	})
+
+	router.GET("/debug/pprof/*", wrapHandler(http.DefaultServeMux))
 
 	registerV1Routes()
 	return router
