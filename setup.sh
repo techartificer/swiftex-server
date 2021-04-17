2de2ac1d8a8a3d1c9b8d79e368643dcdcdf3f1f0
caffeines <sadat.talks@gmail.com>
2021-04-17 09:51:25 +0600

    saving

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # download config file from firestore
-wget -O config.yml https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/config.yml\?alt\=media\&token\=36d9a00a-1ad3-468b-a471-98b205b66c37
+wget -O config.yml https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/config.stage.yml\?alt\=media\&token\=ebd9ef6e-902c-4649-8779-a0eaaf445f5f
 
 # download swiftex-firebase.json from firestore
 wget -O swiftex-firebase.json https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/swiftex-firebase.json\?alt\=media\&token\=91db4d89-de7e-441c-8c79-2267656c0d88
 
 cat wget-log
 
-docker build -t swiftex .
+docker build -t swiftex:1.0 .
 
 docker-compose down
 docker-compose up -d
