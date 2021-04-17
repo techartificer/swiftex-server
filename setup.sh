c04081aa1daa5a46bd66888eb43213adf4b999ee
caffeines <sadat.talks@gmail.com>
2021-04-17 09:42:08 +0600

    fix config file

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-rm wget-log
-
 # download config file from firestore
-wget -O config.yml -b https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/config.yml\?alt\=media\&token\=32652e75-6243-4666-8f1e-e15ac8d049a3
+wget -O config.yml https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/config.yml\?alt\=media\&token\=36d9a00a-1ad3-468b-a471-98b205b66c37
 
 # download swiftex-firebase.json from firestore
-wget -O swiftex-firebase.json -b https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/swiftex-firebase.json\?alt\=media\&token\=91db4d89-de7e-441c-8c79-2267656c0d88
+wget -O swiftex-firebase.json https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/swiftex-firebase.json\?alt\=media\&token\=91db4d89-de7e-441c-8c79-2267656c0d88
 
 cat wget-log
 
@@ -22,3 +19,5 @@ curl -s localhost:4141 | json_pp
 echo ""
 echo ""
 echo "Server is ready..."
+
+docker system prune
