ce4ed20815deb43caa56c7e2501cda3b0b091c01
caffeines <sadat.sayem@tigerit.com>
2021-05-23 01:03:30 +0600

    Change prod config

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # download config file from firestore
-wget -O config.yml https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/config.prod.yml\?alt\=media\&token\=468328e8-96cf-4e36-8151-1b6d9ba58f34
+wget -O config.yml https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/config.prod.yml\?alt\=media\&token\=3e08bdea-abf9-479e-92c7-2bd4e2f9448c
 
 # download swiftex-firebase.json from firestore
 wget -O swiftex-firebase.json https://firebasestorage.googleapis.com/v0/b/swiftexbd.appspot.com/o/swiftex-firebase.json\?alt\=media\&token\=d7ee69b0-0967-48ad-8bb9-9590c223aa94
